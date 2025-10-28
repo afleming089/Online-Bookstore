c174898e5999481c5064cd3d995c6d92b7764542
Anton Fleming <aflemingrocks089@gmail.com>
2025-10-28 15:01:17 -0500

    Added media directory

---
+++ spring-boot-backend/src/main/java/com/mycompany/app/media/Media.java
@@ -0,0 +1,11 @@
+package com.mycompany.app.media;
+
+public interface Media {
+    public String getTitle();
+
+    public String getDescription();
+
+    public String getAuthor();
+
+    public String getPrice();
+}
